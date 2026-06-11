118a4eae84aaf21650e150d90184b54c1ce45d8f
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-11 12:52:45 +0800

    chore(deps): remove @astrojs/cloudflare dependency
    This site will be served via static files and will periodically rebuild
    instead

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 
-import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()],
   },
